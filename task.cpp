b32d79277000b53e4640d01d4f76fb483f507ed0
Arseniy Obolenskiy <gooddoog@student.su>
2019-11-21 13:46:26 +0300

    Swap nested loops
    Tests:
    
    AMD FX-8370E: 8 cores, 8 threads
    
    ./executor.out 10000 10000 100000 128 1 output_10k_10k_100k_128_1
    Execution time = 3.79244 seconds
    Relative difference = 0.000000000000000
    
    ./executor.out 10000 10000 1000000 128 1 output_10k_10k_1m_128_1
    Execution time = 35.12428 seconds
    Relative difference = 0.000000000000000

--- task.cpp
+++ task.cpp
@@ -12,9 +12,9 @@ void performQueries(int32_t nRows, int32_t nCols, int32_t nQueries, int32_t nRes
         int32_t colB = queries[queryId * 4 + 3];
 
         #pragma omp parallel for collapse(2)
-        for (int32_t col = 0; col < nRes; col++)
+        for (int32_t row = 0; row < nRes; row++)
         {
-            for (int32_t row = 0; row < nRes; row++)
+            for (int32_t col = 0; col < nRes; col++)
             {
                 int32_t cRowA = rowA + row;
                 int32_t cColA = colA + col;
